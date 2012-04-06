4e7c6ca805c14b2b66ae7b2f35c7a161bac51f9c
uthiravel <uthiravel@railsfactory.org>
2012-04-06 17:32:10 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def show
+  end
+  
+  def about_us
+  end
+
+  def contact_us
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pandu::Application.config.secret_token = '3c296c9551c1bad61145fbcfcf532d45a89d9fbe8a4eacc6e76a6e7fdfa21d3b4fdc3485d00cd92a6d1aff618c79c1de88ac6dcc2d27c01e1bf1bf80d70ff265'
